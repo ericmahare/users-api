83799c0edd0f998c0800de38df57429e4480b14d
Eric Mahare <erikmahare@gmail.com>
2022-11-18 19:40:17 +0300

    Include cors origin

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const employeeRouter = require('./src/routes/employeeRoutes')
 const app = express()
 
 // middlewares
 app.use(express.json())
+app.use(cors({
+  origin: "*"
+}))
 app.use(employeeRouter)
+
 const port =  process.env.PORT || 8000
 app.listen(port, ()=> {
   console.log(`Listening to port ${port}`)
